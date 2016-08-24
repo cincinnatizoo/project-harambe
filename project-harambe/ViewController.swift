9b6eae7edef186e474a798b618d9e2e6d8352b52
Jonathan <Jonathan@Jonathans-MacBook-Pro.local>
2016-08-23 21:36:20 -0400

    JY - Project Setup

---
+++ project-harambe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project-harambe
+//
+//  Created by Jonathan Yeung on 2016-08-23.
+//  Copyright © 2016 cincinnatizoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
